e4f202d62437bd913351d05872db2b9f5eb3a8d5
Karan Sharma <55722391+ksharma-xyz@users.noreply.github.com>
2025-03-03 23:15:40 +1100

    target sdk 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "xyz.ksharma.tinkandroiddemo"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "xyz.ksharma.tinkandroiddemo"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
